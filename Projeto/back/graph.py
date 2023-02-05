93f349b496948b26d0afab0a71d8076778393968
JoaoPCoelho <japapauloxd@hotmail.com>
2023-02-05 18:49:16 -0300

    Adicionando dificuldade hard e arrumando a logica
    Co-Authored-by: CibeleG <cibelegoudinho13@gmail.com>

--- Projeto/back/graph.py
+++ Projeto/back/graph.py
@@ -1,5 +1,31 @@
 import random
 
+dificulty = input("Escolha a dificuldade de jogo: facil ou dificil\n")
+
+if dificulty == "dificil":
+    graph = {
+        "Kingdom of Merland": {"Principality of Sicocoria": -2, "Kingdom of Tentes": 4, "Douchientian Theocracy": 11},
+        "Principality of Sicocoria": {"Kingdom of Merland": -2, "Principality of Montia": -6},
+        "Principality of Montia": {"Principality of Sicocoria": -6, "Kingdom of Tentes": 2},
+        "Kingdom of Tentes": {"Principality of Montya": 8, "Kingdom of Merland": 4, "Gey": 8, "Republic of Nyirkhad": -7, "Grand Duchy of Lignia": -5},
+        "Gey": {"Kingdom of Tentes": 8, "Republic of Nyirkhad": 5},
+        "Republic of Nyirkhad": {"Gey": 5, "Kingdom of Tentes": -7, "Kingdom of Guileria": 8, "Grand Duchy of Lignia": -9},
+        "Bishopric of Silgulia": {"Lharanha": 10, "United Provinces of Rouertia": -15, "Kingdom of Givrairatil": -7},
+        "United Provinces of Rouertia": {"Lharanha": 5, "Bishopric of Silgulia": -15, "Kingdom of Givairatil": 11, "Douchientian Theocracy": -7},
+        "Kingdom of Givrairatil": {"Bishopric of Silgulia": -7, "United Provinces of Rouertia": 11, "Lharanha": -2, "Douchientian Theocracy": 8},
+        "Lharanha": {"Bishopric of Silgulia": 10, "Kingdom of Givrairatil": -2, "United Provinces of Rouertia": 5, "Douchientian Theocracy": -5},
+        "Douchientian Theocracy": {"Kingdom of Merland": 11, "United Provinces of Rouertia": -7, "Kingdom of Givrairatil": 8, "Lharanha": -5}
+    }
+elif dificulty == "facil":
+    graph = {
+        "Kingdom of Merland": {"Principality of Sicocoria": -2, "Kingdom of Tentes": 4},
+        "Principality of Sicocoria": {"Kingdom of Merland": -2, "Principality of Montia": -6},
+        "Principality of Montia": {"Principality of Sicocoria": -6, "Kingdom of Tentes": 2},
+        "Kingdom of Tentes": {"Principality of Montya": 8, "Kingdom of Merland": 4, "Gey": 8, "Republic of Nyirkhad": -7},
+        "Gey": {"Kingdom of Tentes": 8, "Republic of Nyirkhad": 5},
+        "Republic of Nyirkhad": {"Gey": 5, "Kingdom of Tentes": -7}
+}
+
 def find_path_with_fixed_cost(graph, start, end, path=None, cost=0):
     if path is None:
         path = []
@@ -15,16 +41,8 @@ def find_path_with_fixed_cost(graph, start, end, path=None, cost=0):
             new_paths = find_path_with_fixed_cost(graph, node, end, path, new_cost)
             for new_path, new_cost in new_paths:
                 paths.append((new_path, new_cost))
+    print(paths)
     return paths
-
-graph = {
-    "Kingdom of Merland": {"Principality of Sicocoria": -2, "Kingdom of Tentes": 4},
-    "Principality of Sicocoria": {"Kingdom of Merland": -2, "Principality of Montia": -6},
-    "Principality of Montia": {"Principality of Sicocoria": -6, "Kingdom of Tentes": 2},
-    "Kingdom of Tentes": {"Principality of Montya": 8, "Kingdom of Merland": 4, "Gey": 8, "Republic of Nyirkhad": -7},
-    "Gey": {"Kingdom of Tentes": 8, "Republic of Nyirkhad": 5},
-    "Republic of Nyirkhad": {"Gey": 5, "Kingdom of Tentes": -7}
-}
 
 nodes = list(graph.keys())
 start = random.choice(nodes)
@@ -41,19 +59,22 @@ for node1 in graph:
         cost_map[f"{node1} to {node2}"] = graph[node1][node2]
 
 for cost in cost_map:
-    print("O custo de", cost, "é igual a:", cost_map[cost])
+    print("O custo de", cost, "e igual a:", cost_map[cost])
 
 selected_path = None
 while not selected_path:
-    user_choice = input("Digite o custo do menor caminho possivel: ")
+    min_cost = min(cost for _, cost in results)
+    user_choice = input("Digite o custo do menor caminho possível: ")
     try:
         choice = int(user_choice)
-        for path, cost in results:
-            if cost == choice:
-                selected_path = (path, cost)
-        if not selected_path:
+        if choice == min_cost:
+            for path, cost in results:
+                if cost == choice:
+                    selected_path = (path, cost)
+                    break
+        else:
             print("Este não é o caminho mais curto, tente novamente")
     except ValueError:
-        print("Entrada invalida, digite um numero!")
+        print("Entrada inválida, digite um número!")
 
-print("Parabéns, você acertou, o caminho correto é ", selected_path[0], "com custo de: ", selected_path[1])
+print("Parabens, voce acerto, o caminho correto e ", selected_path[0], "com custo de: ", selected_path[1])
