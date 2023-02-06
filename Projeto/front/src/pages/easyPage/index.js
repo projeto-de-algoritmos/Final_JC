39f2a07619107d4195b647a1e0034433daede2b1
CibeleG <cibelegoudinho13@gmail.com>
2023-02-05 23:37:13 -0300

    Adicionando conexao com o back

--- Projeto/front/src/pages/easyPage/index.js
+++ Projeto/front/src/pages/easyPage/index.js
@@ -6,69 +6,100 @@ import './index.css';
 function EasyPage() {
   const navigate = useNavigate()
   const [answer, setAnswer] = useState('')
+  const [graphAns, setGraphAns] = useState({})
   const [showAnswer, setShowAnswer] = useState(false)
+  const [tryAgain, setTryAgain] = useState(false)
+  const [userAnswer, setUserAnswer] = useState(true)
 
-  function handleChangeAnswer(value){
+  function handleChangeAnswer(value) {
     setAnswer(value)
     console.log(answer)
   }
 
-  function handleSubmitAnswer(){
-    setShowAnswer(true)
+  function handleSubmitAnswer() {
+    if (answer == 30 - graphAns['custo']) {
+      setShowAnswer(true)
+      setUserAnswer(false)
+    }
+    else {
+      setTryAgain(true)
+      setUserAnswer(false)
+    }
   }
 
-//   useEffect(() => {
-//     fetch(`http://localhost:8000/`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       setLoadingResult(true)
-//       setRoute(data)
-//       console.log(data)
-//     });
-//   });
+  function handleSubmitTryAgain() {
+    setTryAgain(false)
+    setUserAnswer(true)
+  }
 
-  const data = {
-    "Kingdom of Tentes": 20,
-    "Gey": -15,
-    "Principality of Montih": 15,
-    "Republic of Nyirkad": 10,
-    "Kingdom of Merland": 5,
-    "Principality of Sicocoria": 3,
+  const getApiData = async () => {
+    await fetch(`http://localhost:8004/easy`)
+      .then((response) => response.json())
+      .then((data) => {
+        setGraphAns(data)
+      });
+  }
+
+  useEffect(() => {
+    getApiData();
+  }, []);
+
+  const data_city = {
+    "Kingdom of Tentes": 12,
+    "Gey": -13,
+    "Principality of Montih": 5,
+    "Republic of Nyirkad": 7,
+    "Kingdom of Merland": -2,
+    "Principality of Sicocoria": 6,
   }
 
   return (
-      <div className='easy-mode'>
-        <div className='map-easy'>
-            <img className='map-easy-image' src={MapImage}/>
-        </div>
-        <div className='info-easy'>
+    <div className='easy-mode'>
+      <div className='map-easy'>
+        <img className='map-easy-image' src={MapImage} />
+      </div>
+      <div className='info-easy'>
+
+        {userAnswer && (
+          <>
             <h2 className='desc'>Valor inicial na carteira: 30</h2>
             <h2 className='desc'>Valor para entrar em cada reino:</h2>
-            <h2 className='desc'>Kingdom of Tentes: {data['Kingdom of Tentes']}</h2>
-            <h2 className='desc'>Gey: {data['Gey']}</h2>
-            <h2 className='desc'>Principality of Montih: {data['Principality of Montih']}</h2>
-            <h2 className='desc'>Republic of Nyirkad: {data['Republic of Nyirkad']}</h2>
-            <h2 className='desc'>Kingdom of Merland: {data['Kingdom of Merland']}</h2>
-            <h2 className='desc'>Principality of Sicocoria: {data['Principality of Sicocoria']}</h2>
-            {!showAnswer ? (
-                <>
-                    <h2 className='desc'>Indo de Kingdom of Tentes para Principality of Sicocoria, quanto de ouro você teria no final?</h2>
-                    <div className='input-container'>
-                        <h2 className='desc'>Valor na carteira: </h2>
-                        <input onChange={(e)=> handleChangeAnswer(e.target.value)}
-                            type="text"  
-                            value={answer} 
-                            name="answer" 
-                            className="input"/>
-                        <button onClick={handleSubmitAnswer} className="button">Enviar</button>
-                    </div>
-                </>
-            ) : (
-                <div></div>
-            )}
-            <button className="button-return" onClick={() => navigate("/")}>Voltar</button>
-        </div>
+            <h2 className='desc'>Kingdom of Tentes: {data_city['Kingdom of Tentes']}</h2>
+            <h2 className='desc'>Gey: {data_city['Gey']}</h2>
+            <h2 className='desc'>Principality of Montih: {data_city['Principality of Montih']}</h2>
+            <h2 className='desc'>Republic of Nyirkad: {data_city['Republic of Nyirkad']}</h2>
+            <h2 className='desc'>Kingdom of Merland: {data_city['Kingdom of Merland']}</h2>
+            <h2 className='desc'>Principality of Sicocoria: {data_city['Principality of Sicocoria']}</h2>
+            <h2 className='desc'>Indo de {graphAns['cidade inicial']} para {graphAns['cidade de destino']}, quanto de ouro você teria no final?</h2>
+            <div className='input-container'>
+              <h2 className='desc'>Valor final na carteira: </h2>
+              <input onChange={(e) => handleChangeAnswer(e.target.value)}
+                type="text"
+                value={answer}
+                name="answer"
+                className="input" />
+              <button onClick={handleSubmitAnswer} className="button">Enviar</button>
+            </div>
+          </>
+        )}
+
+        {tryAgain && (
+          <div className='show-answer'>
+            <h2 className='desc'>Você Errou!</h2>
+            <button onClick={handleSubmitTryAgain} className="button-try-again">Tentar Novamente</button>
+          </div>
+        )}
+
+        {showAnswer && (
+          <>
+            <h2 className='desc'>Você Acertou! O caminho, gastando apenas {graphAns['custo']} e ficando com {30 - graphAns['custo']} na carteira, seria:</h2>
+            <h2 className='desc'>{graphAns['caminho'].join("->")}</h2>
+          </>
+        )}
+
+        <button className="button-return" onClick={() => navigate("/")}>Voltar</button>
       </div>
+    </div>
   );
 }
 
--- Projeto/front/src/pages/hardPage/index.js
+++ Projeto/front/src/pages/hardPage/index.js
@@ -7,88 +7,117 @@ function HardPage() {
   const navigate = useNavigate()
   const [answer, setAnswer] = useState('')
   const [showAnswer, setShowAnswer] = useState(false)
+  const [graphAns, setGraphAns] = useState({})
+  const [tryAgain, setTryAgain] = useState(false)
+  const [userAnswer, setUserAnswer] = useState(true)
 
-  function handleChangeAnswer(value){
+  function handleChangeAnswer(value) {
     setAnswer(value)
     console.log(answer)
   }
 
-  function handleSubmitAnswer(){
-    setShowAnswer(true)
+  function handleSubmitAnswer() {
+    if (answer == 40 - graphAns['custo']) {
+      setShowAnswer(true)
+      setUserAnswer(false)
+    }
+    else {
+      setTryAgain(true)
+      setUserAnswer(false)
+    }
   }
 
-//   useEffect(() => {
-//     fetch(`http://localhost:8000/`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       setLoadingResult(true)
-//       setRoute(data)
-//       console.log(data)
-//     });
-//   });
+  function handleSubmitTryAgain() {
+    setTryAgain(false)
+    setUserAnswer(true)
+  }
 
-  const data = {
-    "Kingdom of Tentes": 20,
-    "Gey": -15,
-    "Principality of Montih": 15,
-    "Republic of Nyirkad": 10,
-    "Kingdom of Merland": 5,
-    "Principality of Sicocoria": 3,
-    "Kingdom of Guileria": -2,
-    "Kingdom of Givrairatil": -4,
-    "Grand Duchy of Lignia": 45,
+  const getApiData = async () => {
+    await fetch(`http://localhost:8004/hard`)
+      .then((response) => response.json())
+      .then((data) => {
+        setGraphAns(data)
+      });
+  }
+
+  useEffect(() => {
+    getApiData();
+  }, []);
+
+  const data_hard = {
+    "Kingdom of Tentes": 30,
+    "Gey": -25,
+    "Principality of Montih": 43,
+    "Republic of Nyirkad": 4,
+    "Kingdom of Merland": 1,
+    "Principality of Sicocoria": -3,
+    "Kingdom of Guileria": 12,
+    "Kingdom of Givrairatil": -24,
+    "Grand Duchy of Lignia": 40,
     "Douchientian Theocracy": -37,
-    "United Provinces of Rouertia": -1,
-    "Bishopric of Silgulia": 35,
+    "United Provinces of Rouertia": 31,
+    "Bishopric of Silgulia": 37,
     "Lharanha": -2
   }
 
   return (
-      <div className='hard-mode'>
-        <div className='map'>
-            <img className='map-image' src={MapImage}/>
-        </div>
-        <div className='info'>
+    <div className='hard-mode'>
+      <div className='map'>
+        <img className='map-image' src={MapImage} />
+      </div>
+      <div className='info'>
+        {userAnswer && (
+          <>
             <h2 className='desc'>Valor inicial na carteira: 30</h2>
             <h2 className='desc'>Valor para entrar em cada reino:</h2>
             <div className='info-container'>
-                <div className='left-info'>
-                    <h2 className='desc'>Kingdom of Tentes: {data['Kingdom of Tentes']}</h2>
-                    <h2 className='desc'>Gey: {data['Gey']}</h2>
-                    <h2 className='desc'>Principality of Montih: {data['Principality of Montih']}</h2>
-                    <h2 className='desc'>Republic of Nyirkad: {data['Republic of Nyirkad']}</h2>
-                    <h2 className='desc'>Kingdom of Merland: {data['Kingdom of Merland']}</h2>
-                    <h2 className='desc'>Principality of Sicocoria: {data['Principality of Sicocoria']}</h2>
-                </div>
-                <div className='right-info'>
-                    <h2 className='desc'>Kingdom of Guileria: {data['Kingdom of Guileria']}</h2>
-                    <h2 className='desc'>United Provinces of Rouertia: {data['United Provinces of Rouertia']}</h2>
-                    <h2 className='desc'>Kingdom of Givrairatil: {data['Kingdom of Givrairatil']}</h2>
-                    <h2 className='desc'>Douchientian Theocracy: {data['Douchientian Theocracy']}</h2>
-                    <h2 className='desc'>Grand Duchy of Lignia: {data['Grand Duchy of Lignia']}</h2>
-                    <h2 className='desc'>Lharanha: {data['Lharanha']}</h2>
-                    <h2 className='desc'>Bishopric of Silgulia: {data['Bishopric of Silgulia']}</h2>
-                </div>
+              <div className='left-info'>
+                <h2 className='desc'>Kingdom of Tentes: {data_hard['Kingdom of Tentes']}</h2>
+                <h2 className='desc'>Gey: {data_hard['Gey']}</h2>
+                <h2 className='desc'>Principality of Montih: {data_hard['Principality of Montih']}</h2>
+                <h2 className='desc'>Republic of Nyirkad: {data_hard['Republic of Nyirkad']}</h2>
+                <h2 className='desc'>Kingdom of Merland: {data_hard['Kingdom of Merland']}</h2>
+                <h2 className='desc'>Principality of Sicocoria: {data_hard['Principality of Sicocoria']}</h2>
+              </div>
+              <div className='right-info'>
+                <h2 className='desc'>Kingdom of Guileria: {data_hard['Kingdom of Guileria']}</h2>
+                <h2 className='desc'>United Provinces of Rouertia: {data_hard['United Provinces of Rouertia']}</h2>
+                <h2 className='desc'>Kingdom of Givrairatil: {data_hard['Kingdom of Givrairatil']}</h2>
+                <h2 className='desc'>Douchientian Theocracy: {data_hard['Douchientian Theocracy']}</h2>
+                <h2 className='desc'>Grand Duchy of Lignia: {data_hard['Grand Duchy of Lignia']}</h2>
+                <h2 className='desc'>Lharanha: {data_hard['Lharanha']}</h2>
+                <h2 className='desc'>Bishopric of Silgulia: {data_hard['Bishopric of Silgulia']}</h2>
+              </div>
             </div>
-            {!showAnswer ? (
-                <>
-                    <h2 className='desc'>Indo de Kingdom of Tentes para Kingdom of Givrairatil, quanto de ouro você teria no final?</h2>
-                    <div className='input-container'>
-                        <h2 className='desc'>Valor na carteira: </h2>
-                        <input onChange={(e)=> handleChangeAnswer(e.target.value)}
-                            type="text"  
-                            value={answer} 
-                            name="answer" 
-                            className="input"/>
-                        <button onClick={handleSubmitAnswer} className="button">Enviar</button>
-                    </div>
-                </>
-            ) : (
-                <div></div>
-            )}
-            <button className="button-return" onClick={() => navigate("/")}>Voltar</button>
-        </div>
+            <h2 className='desc'>Indo de Kingdom of Tentes para Kingdom of Givrairatil, quanto de ouro você teria no final?</h2>
+            <div className='input-container'>
+              <h2 className='desc'>Valor na carteira: </h2>
+              <input onChange={(e) => handleChangeAnswer(e.target.value)}
+                type="text"
+                value={answer}
+                name="answer"
+                className="input" />
+              <button onClick={handleSubmitAnswer} className="button">Enviar</button>
+            </div>
+          </>
+        )}
+
+        {showAnswer && (
+          <div className='show-answer-hard'>
+            <h2 className='desc'>Você Acertou! O caminho, gastando apenas {graphAns['custo']} e ficando com {40 - graphAns['custo']} na carteira, seria:</h2>
+            <h2 className='desc'>{graphAns['caminho'].join("->")}</h2>
+          </div>
+        )}
+
+        {tryAgain && (
+          <div className='try-again-hard'>
+            <h2 className='desc'>Você Errou!</h2>
+            <button onClick={handleSubmitTryAgain} className="button-try-again">Tentar Novamente</button>
+          </div>
+        )}
+        <button className="button-return" onClick={() => navigate("/")}>Voltar</button>
       </div>
+    </div>
   );
 }
 
