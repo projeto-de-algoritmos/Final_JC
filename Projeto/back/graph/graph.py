e9cffb2710d65a55ca8be15ce88cf082d8180902
JoaoPCoelho <japapauloxd@hotmail.com>
2023-02-06 14:42:58 -0300

    Adicionando peso aos nos do grafo dificil

--- Projeto/back/graph/graph.py
+++ Projeto/back/graph/graph.py
@@ -21,24 +21,26 @@ def graph(dificulty):
 
     if dificulty == "dificil":
         graph = {
-            "Kingdom of Merland": {"Principality of Sicocoria": -2, "Kingdom of Tentes": 4, "Douchientian Theocracy": 11},
-            "Principality of Sicocoria": {"Kingdom of Merland": -2, "Principality of Montia": -6},
-            "Principality of Montia": {"Principality of Sicocoria": -6, "Kingdom of Tentes": 2},
-            "Kingdom of Tentes": {"Principality of Montya": 8, "Kingdom of Merland": 4, "Gey": 8, "Republic of Nyirkhad": -7, "Grand Duchy of Lignia": -5},
-            "Gey": {"Kingdom of Tentes": 8, "Republic of Nyirkhad": 5},
-            "Republic of Nyirkhad": {"Gey": 5, "Kingdom of Tentes": -7, "Kingdom of Guileria": 8, "Grand Duchy of Lignia": -9},
-            "Bishopric of Silgulia": {"Lharanha": 10, "United Provinces of Rouertia": -15, "Kingdom of Givrairatil": -7},
-            "United Provinces of Rouertia": {"Lharanha": 5, "Bishopric of Silgulia": -15, "Kingdom of Givairatil": 11, "Douchientian Theocracy": -7},
-            "Kingdom of Givrairatil": {"Bishopric of Silgulia": -7, "United Provinces of Rouertia": 11, "Lharanha": -2, "Douchientian Theocracy": 8},
-            "Lharanha": {"Bishopric of Silgulia": 10, "Kingdom of Givrairatil": -2, "United Provinces of Rouertia": 5, "Douchientian Theocracy": -5},
-            "Douchientian Theocracy": {"Kingdom of Merland": 11, "United Provinces of Rouertia": -7, "Kingdom of Givrairatil": 8, "Lharanha": -5}
+            "Kingdom of Merland": {"Principality of Sicocoria": (-9, -3), "Principality of Montia": (-6, 43), "Douchientian Theocracy": (8, -37), "Grand Duchy of Lignia": (-3, 40), "Republic of Nyirkhad": (28, 4)},
+            "Principality of Sicocoria": {"Kingdom of Merland": (-9, 1), "Principality of Montia": (17, 43), "Douchientian Theocracy": (7, -37)},
+            "Principality of Montia": {"Principality of Sicocoria": (17, -3), "Kingdom of Tentes": (-4, 30), "Kingdom of Merland": (-6, 1), "Grand Duchy of Lignia": (20, 40), "Kingdom of Guileria": (-25, 12)},
+            "Kingdom of Tentes": {"Principality of Montia": (-4, 43), "Gey": (45, -25), "Republic of Nyirkhad": (35, 4)},
+            "Gey": {"Kingdom of Tentes": (45, 30), "Republic of Nyirkhad": (-3, 4), "Douchientian Theocracy": (37, -37)},
+            "Republic of Nyirkhad": {"Gey": (-3, -25), "Kingdom of Tentes": (35, 30), "Kingdom of Givrairatil": (-33, -24), "Kingdom of Merland": (28, 1)},
+            "Bishopric of Silgulia": {"Lharanha": (-5, -2), "United Provinces of Rouertia": (12, 31), "Douchientian Theocracy": (-2, -37)},
+            "United Provinces of Rouertia": { "Bishopric of Silgulia": (12, 37), "Kingdom of Givrairatil": (-5, -24), "Douchientian Theocracy": (25, -37), "Kingdom of Guileria": (2, 12)},
+            "Kingdom of Givrairatil": {"United Provinces of Rouertia": (-5, 31), "Kingdom of Guileria": (4, 12), "Republic of Nyirkhad": (-33, 4)},
+            "Lharanha": {"Bishopric of Silgulia": (-5, 37), "Douchientian Theocracy": (11, -37)},
+            "Douchientian Theocracy": {"Principality of Sicocoria": (7, -3), "United Provinces of Rouertia": (25, 31), "Bishopric of Silgulia": (-2, 37), "Lharanha": (11, -2), "Kingdom of Guileria": (15, 12), "Grand Duchy of Lignia": (10, 40), "Gey": (37, -25), "Kingdom of Merland": (8, 1)},
+            "Grand Duchy of Lignia":  {"Douchientian Theocracy": (10, -37), "Kingdom of Merland": (-3, 1), "Principality of Montia": (20, 43)},
+            "Kingdom of Guileria": {"Kingdom of Givrairatil": (4, -24), "Douchientian Theocracy": (15, -37), "Principality of Montia": (-25, 43)}
         }
     elif dificulty == "facil":
         graph = {
             "Kingdom of Merland": {"Principality of Sicocoria": (8, 6), "Kingdom of Tentes": (8, 12), "Republic of Nyirkhad": (-1, 7)},
             "Principality of Sicocoria": {"Kingdom of Merland": (8, -2), "Principality of Montia": (9, 5), "Gey": (-11, -13)},
             "Principality of Montia": {"Principality of Sicocoria": (9, 6), "Kingdom of Tentes": (-7, 12), "Republic of Nyirkhad": (10, 7)},
-            "Kingdom of Tentes": {"Principality of Montya": (-7, 5), "Kingdom of Merland": (8, -2), "Gey": (15, -13)},
+            "Kingdom of Tentes": {"Principality of Montia": (-7, 5), "Kingdom of Merland": (8, -2), "Gey": (15, -13)},
             "Gey": {"Kingdom of Tentes": (15, 12), "Republic of Nyirkhad": (4, 7), "Principality of Sicocoria": (-11, 6)},
             "Republic of Nyirkhad": {"Gey": (4, -13), "Kingdom of Merland": (-1, -2), "Principality of Montia": (10, 5)}
         }
