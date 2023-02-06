d2942dcfff76ec245648ed9366917c79bdc7c05f
João Paulo Coelho <40813329+JoaoP-Coelho@users.noreply.github.com>
2023-02-05 23:34:46 -0300

    Adicionando algoritmo da mochila e arrumando grafo
    Arrumando grafo de jogo facil

--- Projeto/back/graph/graph.py
+++ Projeto/back/graph/graph.py
@@ -1,21 +1,20 @@
 import random
 
-def find_path_with_fixed_cost(graph, start, end, path=None, cost=0):
+def find_path_with_fixed_cost_and_value(graph, start, end, path=None, cost=0, value=0):
     if path is None:
         path = []
     path = path + [start]
     if start == end:
-        return [(path, cost)]
+        return [(path, cost, value)]
     if start not in graph:
         return []
     paths = []
     for node in graph[start]:
         if node not in path:
-            new_cost = cost + graph[start][node]
-            new_paths = find_path_with_fixed_cost(graph, node, end, path, new_cost)
-            for new_path, new_cost in new_paths:
-                paths.append((new_path, new_cost))
-    print(paths)
+            new_cost, new_value = graph[start][node]
+            new_paths = find_path_with_fixed_cost_and_value(graph, node, end, path, cost + new_cost, value + new_value)
+            for new_path, new_cost, new_value in new_paths:
+                paths.append((new_path, new_cost, new_value))
     return paths
 
 def graph(dificulty):
@@ -36,38 +35,35 @@ def graph(dificulty):
         }
     elif dificulty == "facil":
         graph = {
-            "Kingdom of Merland": {"Principality of Sicocoria": -2, "Kingdom of Tentes": 4},
-            "Principality of Sicocoria": {"Kingdom of Merland": -2, "Principality of Montia": -6},
-            "Principality of Montia": {"Principality of Sicocoria": -6, "Kingdom of Tentes": 2},
-            "Kingdom of Tentes": {"Principality of Montya": 8, "Kingdom of Merland": 4, "Gey": 8, "Republic of Nyirkhad": -7},
-            "Gey": {"Kingdom of Tentes": 8, "Republic of Nyirkhad": 5},
-            "Republic of Nyirkhad": {"Gey": 5, "Kingdom of Tentes": -7}
-    }
-
-
+            "Kingdom of Merland": {"Principality of Sicocoria": (8, 6), "Kingdom of Tentes": (8, 12), "Republic of Nyirkhad": (-1, 7)},
+            "Principality of Sicocoria": {"Kingdom of Merland": (8, -2), "Principality of Montia": (9, 5), "Gey": (-11, -13)},
+            "Principality of Montia": {"Principality of Sicocoria": (9, 6), "Kingdom of Tentes": (-7, 12), "Republic of Nyirkhad": (10, 7)},
+            "Kingdom of Tentes": {"Principality of Montya": (-7, 5), "Kingdom of Merland": (8, -2), "Gey": (15, -13)},
+            "Gey": {"Kingdom of Tentes": (15, 12), "Republic of Nyirkhad": (4, 7), "Principality of Sicocoria": (-11, 6)},
+            "Republic of Nyirkhad": {"Gey": (4, -13), "Kingdom of Merland": (-1, -2), "Principality of Montia": (10, 5)}
+        }
 
     nodes = list(graph.keys())
     start = random.choice(nodes)
     end = random.choice(nodes)
-    while start == end:
-        end = random.choice(nodes)
 
-    results = find_path_with_fixed_cost(graph, start, end)
+    results = find_path_with_fixed_cost_and_value(graph, start, end)
 
-    print("Cidade inicial:", start)
-    print("Cidade de destino:", end)
+    max_value = max(value for _, _, value in results)
+
+    bag = max(value for _, _, value in results)
+    selected_path2 = [path for path, _, value in results if value == bag]
+
+    selected_path = [path for path, _, value in results if value == max_value]
 
     cost_map = {}
-    for node1 in graph:
-        for node2 in graph[node1]:
-            cost_map[f"{node1} to {node2}"] = graph[node1][node2]
+        for node1 in graph:
+            for node2 in graph[node1]:
+                cost_map[f"{node1} to {node2}"] = graph[node1][node2]
 
-    for cost in cost_map:
-        print("O custo de", cost, "e igual a:", cost_map[cost])
+        min_cost = min(cost for _, cost in results)
 
-    min_cost = min(cost for _, cost in results)
+        for min_cost in results:
+            selected_path = (min_cost)
 
-    for min_cost in results:
-        selected_path = (min_cost)
-
-    return{"caminho": selected_path[0], "custo": selected_path[1], "cidade inicial": start, "cidade de destino": end}
+        return{"caminho de menor esforco": selected_path[0], "caminho mais rentavel": selected_path2, "maior valor": bag, "custo": selected_path[1], "cidade inicial": start, "cidade de destino": end}
